26a071c3cebfe2a5541f2a2de8181d70089599a1
Antione Mace <amace@sargent-disc.com>
2018-05-02 18:05:15 +0100

    testCases update

--- test/index.js
+++ test/index.js
@@ -14,17 +14,36 @@ const testCases = [
   },
 ]
 
+function loadCalendar(calculator) {
+  var computeDate = new Date();
+  var days = 1;
+  console.log("\nTest 3/3: Uk, 1 year")
+  while (days != 365) {
+    var result =  calculator.getTaxYear(computeDate);
+    console.log(`\n${days + 1}/365`)
+    console.log("Result:", result)
+    computeDate.setDate(computeDate.getDate() + 1);
+    days++;
+  }
+}
+
+
 function runTest() {
   const calculator = taxYear()
 
-  console.log("Test 1/2: default UK, financial year start")
-  console.log("Result:", calculator.getTaxYear(new Date()))
-  console.log("\nTest 2/2: Irish, financial year start")
-  calculator.setFYS({
+  console.log("Test 1/3: Irish, financial year start")
+  console.log("Result:", calculator.getTaxYear(new Date(), {
     day: 1,
     month: 1
+  }))
+  console.log("\nTest 2/3: Uk, financial year start")
+  calculator.setFYS({
+    day: 6,
+    month: 4
   })
   console.log("Result:", calculator.getTaxYear(new Date()))
+
+  loadCalendar(calculator)
 }
 
 runTest()
